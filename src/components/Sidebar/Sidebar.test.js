79db42fbee499c0c6fce18fd4f933412aa4aef39
Chris Saden <chris.saden@udacity.com>
2017-10-10 15:09:37 -0700

    fix(tests): fix Sidebar tests

--- src/components/Sidebar/Sidebar.test.js
+++ src/components/Sidebar/Sidebar.test.js
@@ -1,34 +1,24 @@
 import _ from 'lodash';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Link} from 'react-router-dom';
+import {NavLink} from 'react-router-dom';
 import {shallow} from 'enzyme';
 
 import Sidebar from './Sidebar';
 
 const categories = [];
 const div = document.createElement('div');
-let props = {
-  categories,
-  onSelectCategory: jest.fn(),
-  selected: null
-}
+let props = {categories}
 
 it('renders without crashing', () => {
   ReactDOM.render(<Sidebar {...props}/>, div);
 });
 
-it('renders a link with an active class', () => {
+it('renders a Nav Link', () => {
   categories.push({name: 'foo', path: '/foo'});
-  props = _.assign({}, props, {selected: 'foo'});
   const wrapper = shallow(<Sidebar {...props}/>);
-  expect(wrapper.find(Link).length).toBe(1);
-  expect(wrapper.find(Link).prop('className')).toEqual('category active');
+  const link = wrapper.find(NavLink);
+  expect(link.length).toBe(1);
+  expect(link.prop('className')).toEqual('category');
   expect(wrapper.find('h1').text()).toEqual('chatr');
-});
-
-it('calls a onSelectCategory when link is clicked', () => {
-  const wrapper = shallow(<Sidebar {...props}/>);
-  wrapper.find(Link).simulate('click');
-  expect(props.onSelectCategory).toHaveBeenCalledWith('foo');
 });
