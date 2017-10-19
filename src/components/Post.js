b4a334ee00d265e8287feee5e6bbf563533b492a
Chris Saden <chris.saden@udacity.com>
2017-10-18 18:44:50 -0700

    feature(Post): add edit and delete btns to Post

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,23 +1,78 @@
 import PropTypes from 'prop-types';
 import React, {PureComponent} from 'react';
+import {connect} from 'react-redux';
 import {Link} from 'react-router-dom';
 
+import {deletePost, editPost} from '../redux/reducers/posts';
 import Content from './Content';
+import PostForm from './PostForm';
 import {post} from './app-prop-types';
 import './Post.css';
 
-export default class Post extends PureComponent {
+class Post extends PureComponent {
   static propTypes = {
     numComments: PropTypes.number,
     post: post
   }
 
+  state = {
+    isEditingPost: false
+  }
+
+  handleCancelClick = (e) => {
+    e.preventDefault();
+    this.setState({isEditingPost: false});
+  }
+
+  handleEditClick = (e) => {
+    e.preventDefault();
+    this.setState({isEditingPost: true});
+  }
+
+  handleDeleteClick = (e) => {
+    e.preventDefault();
+    const isRemoved = window.confirm('Are you sure you want to delete this post?');
+    if (isRemoved) {
+      const {post} = this.props;
+      const {id} = post;
+      this.props.deletePost(id);
+    }
+  }
+
+  handleSaveEditedClick = (post) => {
+    this.props.editPost(post);
+    this.setState({isEditingPost: false});
+  }
+
   render() {
     const {numComments, post} = this.props;
+    const {isEditingPost} = this.state;
 
     return (
       <li className='post'>
-        <Content data={post} isPost={true}/>
+        {isEditingPost ?
+          <PostForm
+            data={post}
+            isPost={true}
+            onSubmit={this.handleSaveEditedClick}
+            onCancel={this.handleCancelClick}/>
+          :
+          <Content data={post} isPost={true}/>
+        }
+        {!isEditingPost &&
+          <div className='buttons'>
+            <button
+              className='post-btn__edit'
+              onClick={this.handleEditClick}>
+                edit
+            </button>
+            <button
+              className='post-btn__delete'
+              onClick={this.handleDeleteClick}>
+                delete
+            </button>
+          </div>
+        }
         <div className='post-details'>
           <Link to={`/${post.category}/${post.id}`} className='view-details'>View Details</Link>
           <span className='count-comments'>{numComments} comments</span>
@@ -26,3 +81,10 @@ export default class Post extends PureComponent {
     );
   }
 }
+
+const PostRedux = connect(null, {deletePost, editPost})(Post);
+
+export {
+  PostRedux as default,
+  Post
+};
